6f3c0d5380921dac7246dac9979c64076a2103ba
Artem Sorokin <102421671+YL-bot@users.noreply.github.com>
2023-02-27 22:35:12 +0300

    asdasd

--- main.py
+++ main.py
@@ -25,3 +25,7 @@ def table():
         data = [job.job, team_leader, time, job.collaborators, job.is_finished]
 
     return render_template('works.html', jobs=data)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
